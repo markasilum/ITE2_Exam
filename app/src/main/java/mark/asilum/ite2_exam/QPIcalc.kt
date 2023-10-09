7b5644ad75b22bfc9d25be657ad71d02f1232a55
markasilum <markasilum13@gmail.com>
2023-10-09 14:29:36 +0800

    qpi calculation

--- app/src/main/java/mark/asilum/ite2_exam/QPIcalc.kt
+++ app/src/main/java/mark/asilum/ite2_exam/QPIcalc.kt
@@ -13,65 +13,45 @@ class QPIcalc : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qpicalc)
 
-        val btnA: Button
-        btnA = findViewById(R.id.btnA)
-        val btnBP: Button
-        btnBP = findViewById(R.id.btnBP)
-        val btnB: Button
-        btnB = findViewById(R.id.btnB)
-        val btnCP: Button
-        btnCP = findViewById(R.id.btnCP)
-        val btnC: Button
-        btnC = findViewById(R.id.btnC)
-        val btnF: Button
-        btnF = findViewById(R.id.btnF)
-        val btnD: Button
-        btnD = findViewById(R.id.btnD)
-        val btnU3: Button
-        btnU3 = findViewById(R.id.btnU3)
+        val btnA: Button = findViewById(R.id.btnA)
+        val btnBP: Button = findViewById(R.id.btnBP)
+        val btnB: Button = findViewById(R.id.btnB)
+        val btnCP: Button = findViewById(R.id.btnCP)
+        val btnC: Button = findViewById(R.id.btnC)
+        val btnF: Button = findViewById(R.id.btnF)
+        val btnD: Button = findViewById(R.id.btnD)
+        val btnU3: Button = findViewById(R.id.btnU3)
+        val btnU5: Button = findViewById(R.id.btnU5)
+        val btnU9: Button = findViewById(R.id.btnU9)
+
         btnU3.isEnabled = false
-        val btnU5: Button
-        btnU5 = findViewById(R.id.btnU5)
         btnU5.isEnabled = false
-        val btnU9: Button
-        btnU9 = findViewById(R.id.btnU9)
         btnU9.isEnabled = false
 
-        val numEquiv = mapOf(
-            "A" to 4,
-            "B+" to 3.5,
-            "B" to 3,
-            "C+" to 2.5,
-            "C" to 2,
-            "D" to 1,
-            "F" to 0
-        )
+//        val numEquiv = mapOf(
+//            "A" to 4,
+//            "B+" to 3.5,
+//            "B" to 3,
+//            "C+" to 2.5,
+//            "C" to 2,
+//            "D" to 1,
+//            "F" to 0
+//        )
 
-        var latestClickedLetterButton =""
-        var latestClickedNumberButton =0
+        var letterTimesNumberequiv =0.00
+        var recentLetterVal =0.00
+        var totalUnits =0
 
         //get the number equivalent of the letter grade
         //after getting the letter grade, multiply the number equivalent to the unit of that grade
             //unit is the number button
-        fun equiv(){
-            numEquiv.forEach{entry ->
-                Log.d("zzzzz","${entry.value}")
-//                if(entry.key == latestClickedLetterButton){
-//
-//                }
-        }
-
-
-
-
 
         btnA.setOnClickListener{
 
             val etEquation: EditText
             etEquation = findViewById(R.id.etEquation)
             val value = btnA.text.toString()
-            latestClickedLetterButton = value
-            Log.d("ZZZ123","Button clicked is $latestClickedLetterButton")
+            recentLetterVal = 4.0
             if(etEquation.text.toString()=="Equation"){
                 etEquation.setText("($value * ")
             }else{
@@ -82,7 +62,7 @@ class QPIcalc : AppCompatActivity() {
                 }
 
             }
-            equiv()
+
             btnA.isEnabled=false
             btnBP.isEnabled=false
             btnB.isEnabled=false
@@ -93,8 +73,6 @@ class QPIcalc : AppCompatActivity() {
             btnU3.isEnabled = true
             btnU5.isEnabled = true
             btnU9.isEnabled = true
-
-
         }
 
 
@@ -103,6 +81,7 @@ class QPIcalc : AppCompatActivity() {
             val etEquation: EditText
             etEquation = findViewById(R.id.etEquation)
             val value = btnBP.text.toString()
+            recentLetterVal = 3.5
             Log.d("ZZZ123","Save $value and pass to next")
             if(etEquation.text.toString()=="Equation"){
                 etEquation.setText("($value * ")
@@ -130,6 +109,7 @@ class QPIcalc : AppCompatActivity() {
 
             val etEquation: EditText
             etEquation = findViewById(R.id.etEquation)
+            recentLetterVal = 3.0
             val value = btnB.text.toString()
             Log.d("ZZZ123","Save $value and pass to next")
             if(etEquation.text.toString()=="Equation"){
@@ -159,6 +139,8 @@ class QPIcalc : AppCompatActivity() {
             val etEquation: EditText
             etEquation = findViewById(R.id.etEquation)
             val value = btnCP.text.toString()
+            recentLetterVal = 2.5
+
             Log.d("ZZZ123","Save $value and pass to next")
             if(etEquation.text.toString()=="Equation"){
                 etEquation.setText("($value * ")
@@ -187,6 +169,7 @@ class QPIcalc : AppCompatActivity() {
             val etEquation: EditText
             etEquation = findViewById(R.id.etEquation)
             val value = btnC.text.toString()
+            recentLetterVal = 2.0
             if(etEquation.text.toString()=="Equation"){
                 etEquation.setText("($value * ")
             }else{
@@ -212,6 +195,7 @@ class QPIcalc : AppCompatActivity() {
         btnF.setOnClickListener{
 
             val etEquation: EditText
+            recentLetterVal = 0.0
             etEquation = findViewById(R.id.etEquation)
             val value = btnF.text.toString()
             Log.d("ZZZ123","Save $value and pass to next")
@@ -243,6 +227,7 @@ class QPIcalc : AppCompatActivity() {
             etEquation = findViewById(R.id.etEquation)
             val value = btnD.text.toString()
             Log.d("ZZZ123","Save $value and pass to next")
+            recentLetterVal = 1.0
             if(etEquation.text.toString()=="Equation"){
                 etEquation.setText("($value * ")
             }else{
@@ -274,13 +259,17 @@ class QPIcalc : AppCompatActivity() {
             etEquation = findViewById(R.id.etEquation)
             val value = btnU3.text.toString()
             Log.d("ZZZ123","Equation text length: ${etEquation.length()} ")
+            letterTimesNumberequiv =letterTimesNumberequiv+ recentLetterVal * value.toInt()
+            totalUnits = totalUnits + value.toInt()
+            Log.d("ZZZ123","letterTimesNumberequiv: ${letterTimesNumberequiv} ")
+            Log.d("ZZZ123","Equation text length: ${totalUnits} ")
+
             if(etEquation.length()<=6){
                 etEquation.append("$value)")
             }else{
                 etEquation.append("$value)")
             }
-            latestClickedNumberButton = value.toInt()
-            Log.d("ZZZ123","Button clicked is $latestClickedNumberButton")
+
             btnA.isEnabled=true
             btnBP.isEnabled=true
             btnB.isEnabled=true
@@ -301,6 +290,9 @@ class QPIcalc : AppCompatActivity() {
             etEquation = findViewById(R.id.etEquation)
             val value = btnU5.text.toString()
             Log.d("ZZZ123","Equation text length: ${etEquation.length()} ")
+            letterTimesNumberequiv =letterTimesNumberequiv+ recentLetterVal * value.toInt()
+            totalUnits = totalUnits + value.toInt()
+
             if(etEquation.length()<=6){
                 etEquation.append("$value)")
             }else{
@@ -326,6 +318,8 @@ class QPIcalc : AppCompatActivity() {
             etEquation = findViewById(R.id.etEquation)
             val value = btnU9.text.toString()
             Log.d("ZZZ123","Equation text length: ${etEquation.length()} ")
+            letterTimesNumberequiv =letterTimesNumberequiv+ recentLetterVal * value.toInt()
+            totalUnits = totalUnits + value.toInt()
             if(etEquation.length()<=6){
                 etEquation.append("$value)")
             }else{
@@ -371,12 +365,11 @@ class QPIcalc : AppCompatActivity() {
         val btnEq: Button
         btnEq = findViewById(R.id.btnEq)
         btnEq.setOnClickListener{
-            val qpiText: EditText
-            qpiText = findViewById(R.id.etQPI)
-//            numEquiv.forEach{entry -> Log.d("zzzzz","${entry.key} : ${entry.value}")
+            val qpiText: EditText = findViewById(R.id.etQPI)
+            var qpi = letterTimesNumberequiv/totalUnits
+            qpiText.setText(qpi.toString())
         }
 
-        }
 
 
     }
